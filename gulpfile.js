38b31bf6990d5d84d1f70a66189ec4dd574f0154
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2018-09-03 21:40:54 +0200

    chore: Use Gulp and Browserify to autoload web page after saving

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('*.html *.js').on('change', browserSync.reload);
+});
+
