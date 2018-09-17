a30c59a8047ba3b160e9f8ee47f99ab07195bbf0
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2018-09-17 23:03:56 +0200

    lesson11: Create the paddle

--- script.js
+++ script.js
@@ -1,34 +1,130 @@
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
+const CANVAS = document.getElementById('game');
+const CTX = CANVAS.getContext('2d');
 
-setInterval(draw, 10);
+let rightPressed = false;
+let leftPressed = false;
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-const ballRadius = 10;
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 39) {
+        rightPressed = true;
+    } else if (event.keyCode === 37) {
+        leftPressed = true;
+    }
+});
 
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = '#0095DD';
-    ctx.fill();
-    ctx.closePath();
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 39) {
+        rightPressed = false;
+    } else if (event.keyCode === 37) {
+        leftPressed = false;
+    }
+});
+
+function makeBall(position, radius, color) {
+    return {
+        position,
+        radius,
+        color,
+        direction: {
+            x: +1, // Rightwards
+            y: -1, // Upwards
+        },
+        draw: function () {
+            CTX.beginPath();
+            CTX.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2);
+            CTX.fillStyle = this.color;
+            CTX.fill();
+            CTX.closePath();
+        },
+        move: function (distance) {
+            let vector = {
+                x: this.direction.x * Math.abs(distance.x),
+                y: this.direction.y * Math.abs(distance.y)
+            };
+
+            if (this.isTouchingLeftWall(vector) ||
+                this.isTouchingRightWall(vector)
+            ) {
+                this.direction.x *= -1;
+            }
+
+            if (this.isTouchingRoof(vector) ||
+                this.isTouchingFloor(vector)
+            ) {
+                this.direction.y *= -1;
+            }
+
+            this.position.x += this.direction.x * distance.x;
+            this.position.y += this.direction.y * distance.y;
+        },
+        isTouchingLeftWall: function (vector) {
+            return this.position.x + vector.x < this.radius;
+        },
+        isTouchingRightWall: function (vector) {
+            return this.position.x + vector.x > CANVAS.width - this.radius;
+        },
+        isTouchingRoof: function (vector) {
+            return this.position.y + vector.y < this.radius;
+        },
+        isTouchingFloor: function (vector) {
+            return this.position.y + vector.y > CANVAS.height - this.radius;
+        }
+    };
+}
+
+function makePaddle(geometry, color) {
+    const position = {
+        x: CANVAS.width/2 - geometry.width/2,
+        y: CANVAS.height - geometry.height
+    };
+
+    return  {
+        width: geometry.width,
+        height: geometry.height,
+        x: position.x,
+        y: position.y,
+        draw: function () {
+            CTX.beginPath();
+            CTX.rect(this.x, this.y, this.width, this.height);
+            CTX.fillStyle = color;
+            CTX.fill();
+            CTX.closePath();
+        },
+        move: function (distance) {
+            if (rightPressed && this.x < CANVAS.width - this.width) {
+                this.x += distance;
+            } else if (leftPressed && this.x > 0) {
+                this.x -= distance;
+            }
+        }
+    };
+}
+
+let ball = makeBall(
+    {
+        x: CANVAS.width / 2,
+        y: CANVAS.height - 30
+    },
+    10,
+    '#0095DD'
+);
+
+let paddle = makePaddle({
+    width: 75,
+    height: 10
+}, '#0095DD');
+
+function eraseCanvas() {
+    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-
-    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
-        dy = -dy;
-    }
-
-    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
-        dx = -dx;
-    }
-
-    x += dx;
-    y += dy;
+    eraseCanvas();
+    ball.draw();
+    paddle.draw();
+    ball.move({x: 2, y: 2});
+    paddle.move(7);
 }
+
+setInterval(draw, 7);
+
